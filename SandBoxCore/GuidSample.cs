7106f2270a5461e51850c4951df3b56cdb0474b3
Paul Matovich <pmatovich@acculynx.com>
2023-03-03 10:59:07 -0600

    Changes...

---
+++ SandBoxCore/GuidSample.cs
@@ -0,0 +1,39 @@
+﻿using SandBoxCore.DesignPatterns.BehavioralPatterns;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SandBoxCore
+{
+    public class GuidSample
+    {
+
+        public string Name { get; set; } = "Name is: Guid Sample";
+        public string Description { get; set; } = string.Empty;
+
+        public void Run()
+        {
+
+           // var tg = new Guid("");    
+
+            Guid superAdminId = Guid.NewGuid();
+
+            var success = Guid.TryParse(superAdminId.ToString(), out var superAdminGuidId);
+
+            Console.WriteLine($"Was seccessful: {success.ToString()}");
+
+            if (success && superAdminGuidId != Guid.Empty)
+            {
+                Console.WriteLine($"Guid output: {superAdminGuidId.ToString()}");
+
+               // context.Options.Headers.Add("x-super-admin-id", superAdminId.ToString());
+            }
+
+            Console.WriteLine("Done");
+        }
+
+    }
+}
---
+++ SandBoxCore/LanguageExtensions.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SandBoxCore
+{
+    public class LanguageExtensions
+    {
+        public static void Run()
+        {
+            var s = "Hello".Concat(" ").Concat("World").Concat("!");
+            Console.WriteLine(s.Length());
+            Console.WriteLine(new string(s.ToArray()));
+        }
+    }
+}
