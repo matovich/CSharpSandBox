381f2967c8ae81b99e5c8fe2df7b9b25d71c116f
Paul Matovich <pmatovich@acculynx.com>
2023-03-20 10:01:37 -0500

    changes...

--- SandBoxCore/LanguageExtensions.cs
+++ SandBoxCore/LanguageExtensions.cs
@@ -1,4 +1,6 @@
 ﻿
+using LanguageExt;
+
 namespace SandBoxCore
 {
     public class LanguageExtensions
@@ -8,6 +10,24 @@ namespace SandBoxCore
             var s = "Hello".Concat(" ").Concat("World").Concat("!");
             Console.WriteLine(s.Length());
             Console.WriteLine(new string(s.ToArray()));
+
+
+            Console.WriteLine(BuildString());
+        }
+
+        public static string BuildString()
+        {
+            Option<string> firstName = Option<string>.None;
+            Option<string> lastName = "Matovich";
+
+
+            firstName = "Paul";
+
+            //firstName = new string("Hello".Concat(" World").ToArray());
+
+            var stringArray = new[] { firstName.Some(x => x).None(""), lastName.Some(x => x).None("") };
+
+            return $"X{string.Join("-", stringArray)}";
         }
     }
 }
